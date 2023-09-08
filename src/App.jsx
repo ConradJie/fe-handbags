d3e5bf6aa4e1192f7d4828bc6c68f68f676a3138
conradjie <conrad.jie@gmail.com>
2023-09-08 15:33:53 +0200

    Poducten 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 import Button from "./components/Button.jsx";
+import Bag1 from "./assets/bag_1.png"
+import Bag2 from "./assets/bag_2.png"
+import Bag3 from "./assets/bag_3.png"
+import Bag4 from "./assets/bag_4.png"
 
 function clickHandler(e) {
     console.log(e.target);
@@ -24,6 +28,32 @@ function App() {
                     onClickHandle={clickHandler}
                 />
             </nav>
+            <main>
+                <article>
+                    <span>Best seller</span>
+                    <img src={Bag1} alt="bag1"/>
+                    <p>The handy bag</p>
+                    <h4>Є400,-</h4>
+                </article>
+                <article>
+                    <span>Best seller</span>
+                    <img src={Bag2} alt="bag2"/>
+                    <p>The stylist bag</p>
+                    <h4>Є250,-</h4>
+                </article>
+                <article>
+                    <span>New collection</span>
+                    <img src={Bag3} alt="bag3"/>
+                    <p>The simple bag</p>
+                    <h4>Є300,-</h4>
+                </article>
+                <article>
+                    <span>New collection</span>
+                    <img src={Bag4} alt="bag4"/>
+                    <p>The trendy bag</p>
+                    <h4>Є150,-</h4>
+                </article>
+            </main>
         </>
     )
 }
