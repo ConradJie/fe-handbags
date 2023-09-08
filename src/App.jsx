d403725929127f5ddff4b365b293d751c50e0172
conradjie <conrad.jie@gmail.com>
2023-09-08 14:11:35 +0200

    Buttons 1+2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import './App.css'
+import Button from "./components/Button.jsx";
+
+function clickHandler(e) {
+    console.log(e.target);
+}
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <Button
+                    text="to the collection"
+                    onClickHandle={clickHandler}
+                />
+                <Button
+                    text="shop all bags"
+                    onClickHandle={clickHandler}
+                />
+                <Button
+                    text="shop all bags"
+                    disabled={true}
+                    onClickHandle={clickHandler}
+                />
+            </nav>
+        </>
+    )
 }
 
 export default App
